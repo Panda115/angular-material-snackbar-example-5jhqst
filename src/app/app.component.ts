8c42f9371cb67f737ce432869a71c61476a3b766
Panda115 <panda.dev1115@gmail.com>
2022-12-15 01:15:27 +0800

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
-import { Component } from "@angular/core";
-import { MatSnackBar } from "@angular/material/snack-bar";
-import { BasicSnackbarComponent } from "./basic-snackbar/basic-snackbar.component";
+import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { BasicSnackbarComponent } from './basic-snackbar/basic-snackbar.component';
 
 @Component({
-  selector: "my-app",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  data = "This is an example for passing data";
+  data = 'This is an example for passing data';
 
   constructor(private snackBar: MatSnackBar) {}
 
@@ -18,10 +18,10 @@ export class AppComponent {
   showSnackbarAction(content, action) {
     let snack = this.snackBar.open(content, action);
     snack.afterDismissed().subscribe(() => {
-      console.log("This will be shown after snackbar disappeared");
+      console.log('This will be shown after snackbar disappeared');
     });
     snack.onAction().subscribe(() => {
-      console.log("This will be called when snackbar button clicked");
+      console.log('This will be called when snackbar button clicked');
     });
   }
   showSnackbarDuration(content, action, duration) {
@@ -30,14 +30,14 @@ export class AppComponent {
   showSnackbarTopPosition(content, action, duration) {
     this.snackBar.open(content, action, {
       duration: 2000,
-      verticalPosition: "top", // Allowed values are  'top' | 'bottom'
-      horizontalPosition: "center" // Allowed values are 'start' | 'center' | 'end' | 'left' | 'right'
+      verticalPosition: 'top', // Allowed values are  'top' | 'bottom'
+      horizontalPosition: 'center', // Allowed values are 'start' | 'center' | 'end' | 'left' | 'right'
     });
   }
   showSnackbarCssStyles(content, action, duration) {
     let sb = this.snackBar.open(content, action, {
       duration: duration,
-      panelClass: ["custom-style"]
+      panelClass: ['custom-style'],
     });
     sb.onAction().subscribe(() => {
       sb.dismiss();
@@ -46,7 +46,7 @@ export class AppComponent {
   showBasicComponent(message: string, panelClass: string) {
     this.snackBar.openFromComponent(BasicSnackbarComponent, {
       data: this.data,
-      duration: 10000
+      duration: 10000,
     });
   }
 }
